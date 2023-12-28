57873cba752f14751d1274fe53056d28114e0280
ISHIDA <ishida.ayumu@isoroot.jp>
2023-12-28 17:13:54 +0900

    ScrollViewControllerのクラス定義

---
+++ studyScrollView/ScrollViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollViewController.swift
+//  studyScrollView
+//
+//  Created by ishida.a on 2023/12/28.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
